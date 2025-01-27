072cbdecb76c41bc8458b521257f85e30c88a413
nilojypb <danilo.goncalves3@aluno.pb.gov.br>
2025-01-27 12:41:20 -0300

    Create PrincipalScreen.js

---
+++ PrincipalScreen.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+export default function PrincipalScreen() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Bem-vindo à Tela Principal!</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: { flex: 1, justifyContent: 'center', alignItems: 'center' },
+  title: { font
+          
